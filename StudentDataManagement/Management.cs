879470f989c32dd86594310cdbc69a1f8af48b9f
rishiraj007 <rishiraj@mounttalent.com>
2023-06-29 20:17:54 +0530

    [Prabhu]Add: Create lsit of student and add values in it

---
+++ StudentDataManagement/Management.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataManagement
+{
+    internal class Management
+    {
+        public static void Display(Student student)
+        {
+            Console.WriteLine($"Student whose ID is: {student.Id} ,Name: {student.Name} " +
+                $",PhoneNumber: {student.PhoneNumber} ,Address: {student.Address} ,Age: {student.Age}");
+        }
+        public static void AddStudentData(List<Student> studentList)
+        {
+            studentList.Add(new Student(1, "Ram", "9999900000", "Delhi", 25));
+            studentList.Add(new Student(2, "Rohit", "9638527410", "Mangalore", 15));
+            studentList.Add(new Student(3, "Ajay", "8527419630", "Kanpur", 11));
+            studentList.Add(new Student(4, "Sam", "8525643212", "Delhi", 12));
+            studentList.Add(new Student(5, "Rahul", "8000000000", "Shimla", 18));
+            studentList.Add(new Student(6, "Rae", "8574195218", "Lucknow", 15));
+            studentList.Add(new Student(7, "Jack", "8741741741", "Kochi", 20));
+            Console.WriteLine("Records Added");
+        }
+        public static void DisplayStudentData(List<Student> studentList)
+        {
+            foreach (Student student in studentList)
+            {
+                Display(student);
+            }
+        }
+    }
+}
--- StudentDataManagement/Program.cs
+++ StudentDataManagement/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Student Data Management!");
+            List<Student> students = new List<Student>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter option to choose \n1.Add Students \n2.Display Students \n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Management.AddStudentData(students);
+                        break;
+                    case 2:
+                        Management.DisplayStudentData(students);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
